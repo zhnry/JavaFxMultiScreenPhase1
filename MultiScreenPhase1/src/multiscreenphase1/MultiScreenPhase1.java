54c3cbcc0633845cc603dcb95c376e477d745541
Phil O'Connell <ist242spring2015@gmail.com>
2015-11-18 22:47:09 -0500

    Reorganize

--- MultiScreenPhase1/src/multiscreenphase1/MultiScreenPhase1.java
+++ MultiScreenPhase1/src/multiscreenphase1/MultiScreenPhase1.java
@@ -19,7 +19,7 @@ public class MultiScreenPhase1 extends Application {
   
   @Override
   public void start(Stage stage) throws Exception {
-    Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+    Parent root = FXMLLoader.load(getClass().getResource("/fx/screen/FXMLDocument.fxml"));
     
     Scene scene = new Scene(root);
     
