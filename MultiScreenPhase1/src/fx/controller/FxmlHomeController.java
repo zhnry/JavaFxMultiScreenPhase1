0add2c0cb35cc7a5753a7d666ab72b99f04b3338
Phil O'Connell <ist242spring2015@gmail.com>
2015-11-18 23:13:03 -0500

    Added home screen

---
+++ MultiScreenPhase1/src/fx/controller/FxmlHomeController.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fx.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+
+/**
+ * FXML Controller class
+ *
+ * @author Phil O'Connell <pxo4@psu.edu>
+ */
+public class FxmlHomeController implements Initializable {
+  @FXML
+  private Label lblName;
+  @FXML
+  private Button btnLogOff;
+
+  /**
+   * Initializes the controller class.
+   */
+  @Override
+  public void initialize(URL url, ResourceBundle rb) {
+    // TODO
+  }  
+
+  @FXML
+  private void bntLogOffMouseClickedHandler(MouseEvent event) {
+    System.out.println("You clicked the logoff button!");
+  }
+  
+}
