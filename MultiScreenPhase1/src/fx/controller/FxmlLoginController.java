590d040505d786540ea420040917975a40bef929
hzhen <hzhen@hzhen.ad.corp.appnexus.com>
2015-11-19 08:39:51 -0500

    log in

--- MultiScreenPhase1/src/fx/controller/FxmlLoginController.java
+++ MultiScreenPhase1/src/fx/controller/FxmlLoginController.java
@@ -5,14 +5,20 @@
  */
 package fx.controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
 /**
  *
@@ -29,8 +35,16 @@ public class FxmlLoginController implements Initializable {
   }  
 
   @FXML
-  private void btnLoginMouseClickedHandler(MouseEvent event) {
+  private void btnLoginMouseClickedHandler(MouseEvent event) throws IOException {
     System.out.println("You clicked the login button!");
+    Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+    
+    String resource ="/fx/screen/FxmlHome.fxml";
+    FXMLLoader loader = new FXMLLoader(getClass().getResource(resource));
+    Parent homeScreen = (Parent) loader.load();
+    
+    Scene scene = new Scene(homeScreen);
+    stage.setScene(scene);
   }
   
 }
