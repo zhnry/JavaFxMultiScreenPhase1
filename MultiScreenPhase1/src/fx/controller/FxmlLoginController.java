150787ef91d6b263defd8b4d88bf332daa71e042
Phil O'Connell <ist242spring2015@gmail.com>
2015-11-18 22:59:07 -0500

    More for login screen

--- MultiScreenPhase1/src/fx/controller/FxmlLoginController.java
+++ MultiScreenPhase1/src/fx/controller/FxmlLoginController.java
@@ -10,7 +10,9 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 
 /**
  *
@@ -19,17 +21,19 @@ import javafx.scene.control.Label;
 public class FxmlLoginController implements Initializable {
   
   @FXML
-  private Label label;
+  private Button btnLogin;
   
-  @FXML
   private void handleButtonAction(ActionEvent event) {
     System.out.println("You clicked me!");
-    label.setText("Hello World!");
   }
   
   @Override
   public void initialize(URL url, ResourceBundle rb) {
     // TODO
   }  
+
+  @FXML
+  private void bthLoginMouseClickedHandler(MouseEvent event) {
+  }
   
 }
