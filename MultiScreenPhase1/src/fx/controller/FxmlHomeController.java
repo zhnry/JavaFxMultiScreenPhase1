b7350c435db291d24a961ce4feee77b1eee3bb4a
hzhen <hzhen@hzhen.ad.corp.appnexus.com>
2015-11-19 08:52:11 -0500

    log out

--- MultiScreenPhase1/src/fx/controller/FxmlHomeController.java
+++ MultiScreenPhase1/src/fx/controller/FxmlHomeController.java
@@ -5,13 +5,19 @@
  */
 package fx.controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -33,8 +39,16 @@ public class FxmlHomeController implements Initializable {
   }  
 
   @FXML
-  private void bntLogOffMouseClickedHandler(MouseEvent event) {
+  private void bntLogOffMouseClickedHandler(MouseEvent event) throws IOException {
     System.out.println("You clicked the logoff button!");
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+    
+    String resource ="/fx/screen/FxmlLogin.fxml";
+    FXMLLoader loader = new FXMLLoader(getClass().getResource(resource));
+    Parent homeScreen = (Parent) loader.load();
+    
+    Scene scene = new Scene(homeScreen);
+    stage.setScene(scene);
   }
 
   @FXML
